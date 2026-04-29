cfc272f06f60b8bcb643718858d0e9a79170970d
lexaagc <pizdyuk963258@gmail.com>
2026-04-29 11:13:19 +0300

    Update MainActivity.kt

--- app/src/main/java/com/barsik/vpn/MainActivity.kt
+++ app/src/main/java/com/barsik/vpn/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.barsik.vpn
+
 import android.app.Activity
 import android.os.Bundle
-import android.webkit.WebView
-class MainActivity: Activity(){ override fun onCreate(savedInstanceState: Bundle?){ super.onCreate(savedInstanceState); val w=WebView(this); setContentView(w); w.settings.javaScriptEnabled=true; w.addJavascriptInterface(JSBridge(this),'Android'); w.loadUrl('file:///android_asset/index.html') } }
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "Barsik VPN работает 😎"
+        setContentView(text)
+    }
+}

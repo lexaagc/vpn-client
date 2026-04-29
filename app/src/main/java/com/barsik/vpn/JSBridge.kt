7ef79ec3552045e23e63918c5475e3ad24acb089
lexaagc <pizdyuk963258@gmail.com>
2026-04-29 21:49:19 +0300

    Update JSBridge.kt

--- app/src/main/java/com/barsik/vpn/JSBridge.kt
+++ app/src/main/java/com/barsik/vpn/JSBridge.kt
@@ -1,5 +1,20 @@
 package com.barsik.vpn
+
 import android.content.Context
 import android.content.Intent
 import android.webkit.JavascriptInterface
-class JSBridge(val c: Context){ @JavascriptInterface fun connectVPN(link:String){ val i=Intent(c,MyVpnService::class.java); i.putExtra('link',link); c.startService(i)} @JavascriptInterface fun disconnectVPN(){ c.stopService(Intent(c,MyVpnService::class.java)) } }
+
+class JSBridge(private val c: Context) {
+
+    @JavascriptInterface
+    fun connectVPN(link: String) {
+        val i = Intent(c, MyVpnService::class.java)
+        i.putExtra("link", link)
+        c.startService(i)
+    }
+
+    @JavascriptInterface
+    fun disconnectVPN() {
+        c.stopService(Intent(c, MyVpnService::class.java))
+    }
+}
